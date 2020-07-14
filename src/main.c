beb4b1190a93d15b58f78a157e1c99affca20372
Unknown <lojso.gm@gmail.com>
2020-07-14 21:34:58 +0300

    Fill triangle drawing

--- src/main.c
+++ src/main.c
@@ -144,10 +144,16 @@ void render(void)
         draw_rectangle(triangle.points[1].x, triangle.points[1].y, 3, 3, 0xFFFFFF00);
         draw_rectangle(triangle.points[2].x, triangle.points[2].y, 3, 3, 0xFFFFFF00);
 
+        draw_filled_triangle(triangle.points[0].x, triangle.points[0].y,
+                      triangle.points[1].x, triangle.points[1].y,
+                      triangle.points[2].x, triangle.points[2].y,
+                      0xFFFFFFFF);
+
+
         draw_triangle(triangle.points[0].x, triangle.points[0].y,
                       triangle.points[1].x, triangle.points[1].y,
                       triangle.points[2].x, triangle.points[2].y,
-                      0xFF00FF00);
+                      0xFF000000);
     }
 
     array_free(triangles_to_render);
--- src/triangle.c
+++ src/triangle.c
@@ -1,1 +1,93 @@
 #include "triangle.h"
+
+void int_swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void fill_flat_bottom_triangle(int x0, int y0,
+                               int x1, int y1,
+                               int x2, int y2,
+                               uint32_t color)
+{
+    float inv_slope1 = (float)(x1 - x0) / (y1 - y0);
+    float inv_slope2 = (float)(x2 - x0) / (y2 - y0);
+
+    float x_start, x_end;
+    x_start = x0;
+    x_end = x0;
+
+    // Loop all the scanlines
+    for (int y = y0; y <= y2; y++)
+    {
+        draw_line(x_start, y, x_end, y, color);
+        x_start += inv_slope1;
+        x_end += inv_slope2;
+    }
+}
+
+void fill_flat_top_triangle(int x0, int y0,
+                            int x1, int y1,
+                            int x2, int y2,
+                            uint32_t color)
+{
+    float inv_slope1 = (float)(x2 - x0) / (y2 - y0);
+    float inv_slope2 = (float)(x2 - x1) / (y2 - y1);
+
+    float x_start, x_end;
+    x_start = x2;
+    x_end = x2;
+
+    // Loop all the scanlines
+    for (int y = y2; y >= y0; y--)
+    {
+        draw_line(x_start, y, x_end, y, color);
+        x_start -= inv_slope1;
+        x_end -= inv_slope2;
+    }
+}
+
+// Scanline drawing
+void draw_filled_triangle(int x0, int y0,
+                          int x1, int y1,
+                          int x2, int y2,
+                          uint32_t color)
+{
+    //sort vertices
+    if (y0 > y1)
+    {
+        int_swap(&y0, &y1);
+        int_swap(&x0, &x1);
+    }
+    if (y1 > y2)
+    {
+        int_swap(&y1, &y2);
+        int_swap(&x1, &x2);
+    }
+    if (y0 > y1)
+    {
+        int_swap(&y0, &y1);
+        int_swap(&x0, &x1);
+    }
+
+    if (y1 == y2)
+    {
+        fill_flat_bottom_triangle(x0, y0, x1, y1, x2, y2, color);
+        
+    }
+    else if (y0 == y1)
+    {
+        fill_flat_top_triangle(x0, y0, x1, y1, x2, y2, color);
+    }
+    else
+    {
+        int Mx, My;
+        My = y1;
+        Mx = ((float)(x2 - x0) * (y1 - y0)) / (float)(y2 - y0) + x0;
+
+        fill_flat_bottom_triangle(x0, y0, x1, y1, Mx, My, color);
+        fill_flat_top_triangle(x1, y1, Mx, My, x2, y2, color);
+    }
+}
