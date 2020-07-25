2a9ca829222ca63af63926a2a394ff4f02ff2120
Unknown <lojso.gm@gmail.com>
2020-07-26 01:51:38 +0300

    Rotation matrices

--- src/main.c
+++ src/main.c
@@ -104,6 +104,9 @@ void update(void)
     mesh.translation.z = 5.0;
 
     mat4_t scale_matrix = mat4_make_scale(mesh.scale.x, mesh.scale.y, mesh.scale.z);
+    mat4_t rotation_matrix_x = mat4_make_rotation_x(mesh.rotation.x);
+    mat4_t rotation_matrix_y = mat4_make_rotation_y(mesh.rotation.y);
+    mat4_t rotation_matrix_z = mat4_make_rotation_z(mesh.rotation.z);
     mat4_t translation_matrix = mat4_make_translation(mesh.translation.x, mesh.translation.y, mesh.translation.z);
 
     int num_faces = array_length(mesh.faces);
@@ -122,15 +125,12 @@ void update(void)
         {
             vec4_t transformed_vertex = vec4_from_vec3(face_verticies[j]);
 
-            // Scale with matrix;
+            // Transform with matrices
             transformed_vertex = mat4_mul_vec4(scale_matrix, transformed_vertex);
+            transformed_vertex = mat4_mul_vec4(rotation_matrix_x, transformed_vertex);
+            transformed_vertex = mat4_mul_vec4(rotation_matrix_y, transformed_vertex);
+            transformed_vertex = mat4_mul_vec4(rotation_matrix_z, transformed_vertex);
             transformed_vertex = mat4_mul_vec4(translation_matrix, transformed_vertex);
-
-            // Constant rotation
-            // transformed_vertex = vec3_rotate_x(transformed_vertex, mesh.rotation.x);
-            // transformed_vertex = vec3_rotate_y(transformed_vertex, mesh.rotation.y);
-            // transformed_vertex = vec3_rotate_z(transformed_vertex, mesh.rotation.z);
-
 
             transformed_vertices[j] = transformed_vertex;
         }
